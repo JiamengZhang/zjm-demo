f84e86ec649b659a6e3635325ea6d222f96bc91b
871096972@qq.com <871096972@qq.com>
2016-08-11 16:08:37 +0800

    demo

--- form/js/form.js
+++
@@ -1,6 +0,0 @@
-/*
- * @description form
- * @author JiaMeng.Zhang
- * @time 2016.07.06
- */
-
--- form/js/formEditor.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-	$('#editor').wysiwyg();
-	
-	var fonts = ['Serif', 'Sans', 'Arial', 'Arial Black', 'Courier', 'Courier New', 'Comic Sans MS', 'Helvetica', 'Impact', 'Lucida Grande', 'Lucida Sans', 'Tahoma', 'Times','Times New Roman', 'Verdana'];
-    var fontTarget = $('[title=Font]').siblings('.dropdown-menu');
-    $.each(fonts, function (idx, fontName) {
-        fontTarget.append($('<li><a data-edit="fontName ' + fontName +'" style="font-family:\''+ fontName +'\'">'+fontName + '</a></li>'));
-    });
-})
